3c872009d964382a321bcf46f55c87503633df88
Vishwa Randy GW <136871744+vishwagw@users.noreply.github.com>
2025-06-07 06:41:23 +0530

    Create rotate_drone.py

---
+++ drone control from PC with Pythn/rotate_drone.py
@@ -0,0 +1,29 @@
+# rotating the drone:
+from time import sleep
+from pyaidrone.aiDrone import *
+from pyaidrone.deflib import *
+
+ready -= 1
+
+def recieveData(packet):
+  global ready
+  ready = packet[7] & 0x03
+
+if __name__ == '__main__':
+  aidrone = AIDrone(recevieData)
+  aidrone.Open("COM3")   # change to the your ports number
+  aidrone.setOption(0)
+  sleep(0.5)
+  
+  while ready != 0:
+    sleep(0.1)
+                  
+  aidrone.takeoff()
+  sleep(5)
+  aidrone.rotation(90)    # Turn to the right about 90 degree
+  sleep(5)
+  aidrone.rotation(-90) 
+  sleep(5)
+  aidrone.landing()
+  sleep(3)
+  aidrone.Close()
