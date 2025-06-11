72802fd59bdcd126a10fb6d633bdd70011b65b61
Vishwa Randy GW <136871744+vishwagw@users.noreply.github.com>
2025-06-11 06:36:30 +0530

    Update drone_control.py

--- drone_control.py
+++ drone_control.py
@@ -9,3 +9,22 @@ from time import sleep
 from pyirbrain.zerodrone import *
 from pyirbrain.deflib import *
 from pyirbrain.ikeyevent import *
+
+def recieveData(packet):
+  global ready
+  ready = packet [7] & 03
+
+def preprocessing(frame):
+  # Resizing 
+  size = (224, 224)
+  frame_resized = cv2.resize(frame, size, interpolation=cv2.INTER_AREA)
+
+  # image normalization
+  frame_normalized = (frame_resized.astype(np.float32) / 127.0) - 1
+
+  # Reshape image dimensions - reshapes them for prediction.
+  frame_reshaped = frame_normalized.reshape((1, 224, 224, 3))
+
+  return frame_reshaped
+
+
