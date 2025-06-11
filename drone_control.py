aa78c56125266f2214ec7dfe05b81551b663fb9d
Vishwa Randy GW <136871744+vishwagw@users.noreply.github.com>
2025-06-11 06:34:07 +0530

    Create drone_control.py

---
+++ drone_control.py
@@ -0,0 +1,11 @@
+# the drone control by using the technable machine:
+import cv2
+import keras
+import numpy as np
+import requests
+from bs4 import BeautifulSoup
+import threading
+from time import sleep
+from pyirbrain.zerodrone import *
+from pyirbrain.deflib import *
+from pyirbrain.ikeyevent import *
