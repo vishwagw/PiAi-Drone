fe7562c41310040573857e2c8b5bc30cbf3bb3e4
Vishwa Randy GW <136871744+vishwagw@users.noreply.github.com>
2025-06-12 05:51:23 +0530

    Update object_recognition_from_vid.py

--- Model training/object_recognition_from_vid.py
+++ Model training/object_recognition_from_vid.py
@@ -24,5 +24,34 @@ try:
   except OSError:
     pass
 
+video_orig = cv.VideoWriter('./data/object_video.avi', fourcc, 20.0, (SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+frame_count = 0
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("캡쳐 실패")
+            break
+        
+    frame = cv.rotate(frame, cv.ROTATE_180)
+    video_orig.write(frame)  # Save video frame
+    
+    # Step 2: Extract Frames from Video
+    if frame_count % 10 == 0:  # Save every 10th frame as an image
+        image_path = f'./data/frame_{frame_count}.jpg'
+        cv.imwrite(image_path, frame)
+    
+    cv.imshow('Video', frame)
+    frame_count += 1
+    
+    key = cv.waitKey(1)
+    if key == 27: 
+        break
+
+cap.release()
+video_orig.release()
+cv.destroyAllWindows()
+
+
 
 
