cc28e25380676f4187dd2c70ae0b662be720ae23
Vishwa Randy GW <136871744+vishwagw@users.noreply.github.com>
2025-06-12 05:48:44 +0530

    Update object_recognition_from_vid.py

--- Model training/object_recognition_from_vid.py
+++ Model training/object_recognition_from_vid.py
@@ -8,4 +8,21 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 from sklearn.model_selection import train_test_split
 
+# Step 1: Capture Video Footage
+SCREEN_WIDTH = 320
+SCREEN_HEIGHT = 240
+
+cap = cv.VideoCapture("http://[your wifi number]:8091/?action=stream")
+cap.set(3, int(SCREEN_WIDTH))
+cap.set(4, int(SCREEN_HEIGHT))
+
+fourcc = cv.VideoWriter_fourcc(*'XVID')
+    
+try:
+  if not os.path.exists('./data'):
+    os.makedirs('./data')
+  except OSError:
+    pass
+
+
 
