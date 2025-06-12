3dcc87fbdc597e3d26277718b46a7d1cd270dcb3
Vishwa Randy GW <136871744+vishwagw@users.noreply.github.com>
2025-06-12 05:47:54 +0530

    Create object_recognition_from_vid.py

---
+++ Model training/object_recognition_from_vid.py
@@ -0,0 +1,11 @@
+# building a model for object recognition:
+import cv2 as cv
+import os
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from sklearn.model_selection import train_test_split
+
+
