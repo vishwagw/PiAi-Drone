59d07f3d873164e8263f8429802856faa4bb4d2a
Vishwa Randy GW <136871744+vishwagw@users.noreply.github.com>
2025-06-13 20:09:24 +0530

    Update colab_programming.py

--- colab_programming.py
+++ colab_programming.py
@@ -3,3 +3,19 @@ import tensorflow as tf
 import numpy as np
 import cv2
 
+# Load data to be used for training.
+data_dir = 'path/to/directory'
+classes = ['object1', 'object2', 'object3']      #  Name the object you want
+
+images = []
+labels = []
+
+for idx, class_name in enumerate(classes):
+  path = os.path.join(data_dir, class_name)
+  for img in os.listdir(path):
+    img_path = os.path.join(path, img)
+    img = cv2.imread(img_path)
+    img = cv2.resize(img, (224, 224)) # change the size of image
+    images.append(img)
+    labels.append(idx)
+
