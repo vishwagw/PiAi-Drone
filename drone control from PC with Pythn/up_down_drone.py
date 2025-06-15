c4f6abfde61d2c8f03bc357a856932cb0902ab9c
Vishwa Randy GW <136871744+vishwagw@users.noreply.github.com>
2025-06-15 07:55:37 +0530

    Update up_down_drone.py

--- drone control from PC with Pythn/up_down_drone.py
+++ drone control from PC with Pythn/up_down_drone.py
@@ -1,1 +1,10 @@
 # moving the drone up and down:
+rom time import sleep
+from pyaidrone.aiDrone import *
+from pyaidrone.deflib import *
+       
+ready = -1
+       
+def receiveData(packet):
+    global ready
+    ready = packet[7] & 0x03
